43786c38a40466b72855d0e60fcf8fd977f6abd2
Maximilian Orlowski <orlowskm@th-brandenburg.de>
2023-11-10 14:19:21 +0100

    set guard for openai base env var

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
 #if not os.environ['OPENAI_API_BASE']:
 #    os.environ['OPENAI_API_BASE'] = "http://149.11.242.18:16598/v1"
-os.environ['OPENAI_API_BASE'] = "http://31.12.82.146:10242/v1"
+if not os.environ['OPENAI_API_BASE']:
+    os.environ['OPENAI_API_BASE'] = "http://31.12.82.146:10242/v1"
 
 os.environ['OPENAI_API_KEY'] = "EMPTY"
 
