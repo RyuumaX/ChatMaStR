382661ce5a7d0930974aa37642c01df001359099
Maximilian Orlowski <Orlowski_Max@web.de>
2024-01-09 00:49:40 +0100

    changed query for document retrieval

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def configure_retriever():
 
     # load persisted vectorstore
     vectorstore = Chroma(persist_directory="./KnowledgeBase/chroma_db", embedding_function=embedding)
-    docs = vectorstore.similarity_search(query="Was ist ein Balkonkraftwerk?", k=3)
+    docs = vectorstore.similarity_search(query="Wer ist registrierungspflichtig?")
     st.session_state["messages"] = [ChatMessage(role="assistant", content=len(docs))]
     retriever = vectorstore.as_retriever()
 
