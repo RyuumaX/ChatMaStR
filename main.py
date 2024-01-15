dc7ddccd9f58f29d60000f6336f2c5ea5abbc30c
Maximilian Orlowski <Orlowski_Max@web.de>
2024-01-16 00:09:51 +0100

    removed gpu kwarg from configure_retriever method again. Only for the time being since it requires a fix on the server first to work with gpu.

--- main.py
+++ main.py
@@ -49,8 +49,7 @@ class PrintRetrievalHandler(BaseCallbackHandler):
 @st.cache_resource(ttl="1h")
 def configure_retriever():
     embedding = HuggingFaceEmbeddings(
-        model_name="T-Systems-onsite/german-roberta-sentence-transformer-v2",
-        model_kwargs={'device': 'cuda:1'}
+        model_name="T-Systems-onsite/german-roberta-sentence-transformer-v2"
     )
 
     # load persisted vectorstore
