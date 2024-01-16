effaa876aac3918087116d47aea0151f1d61c3ef
Maximilian Orlowski <Orlowski_Max@web.de>
2024-01-16 22:28:44 +0100

    removed unused import

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
-from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain.schema import ChatMessage
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.memory import ConversationBufferMemory
