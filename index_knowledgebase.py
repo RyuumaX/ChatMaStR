52e6a388c126792f94f6ced31c41cdfd0aec6ff5
Maximilian Orlowski <Orlowski_Max@web.de>
2024-04-02 23:36:58 +0200

    added some debug info

--- index_knowledgebase.py
+++ index_knowledgebase.py
@@ -16,7 +16,7 @@ def get_pdf_docs_from_path(path):
         if isfile(join(path, f)) and f.endswith(".pdf"):
             documents.extend(PyPDFLoader(join(path, f)).load())
     print(f"Anzahl PDF-docs: {len(documents)}")
-    print(documents[:3])
+    print(documents[:2])
     return documents
 
 
