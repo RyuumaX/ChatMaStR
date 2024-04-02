8521f526af2fe93cc89266ab75c33e2a73e8370f
Maximilian Orlowski <Orlowski_Max@web.de>
2024-04-02 23:58:30 +0200

    fixed indexing bug in create_vectordb function

--- index_knowledgebase.py
+++ index_knowledgebase.py
@@ -58,7 +58,7 @@ def create_vectordb_for_texts(texts, save_path):
     test_embedding = embedding_model.embed_documents(texts[0].page_content)
     n = 5
     print(f"==========first embedding ({len(test_embedding[:n])} of {len(test_embedding)} dimensions:=========")
-    print(test_embedding[:n], "...")
+    print(test_embedding[0][:n], "...")
     vectorstore = Chroma.from_documents(collection_name="small_chunks", documents=texts, embedding=embedding_model,
                                         persist_directory=save_path)
     return vectorstore
